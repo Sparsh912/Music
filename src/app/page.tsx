70a004afbdf0547255a1cf6dbe629e9d2f2765d4
sparsh912 <sparshmaheshwari0908@gmail.com>
2025-09-16 17:20:05 +0530

    add upcomingwebinar section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinar from "@/components/UpcomingWebinar";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <FeaturedCourses/>
       <WhyChooseUs />
       <MusicSchoolTestimonials/>
+      <UpcomingWebinar/>
     </main>
   );
 }
